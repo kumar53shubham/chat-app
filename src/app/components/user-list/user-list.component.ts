d7507a06bedc11e6b08e2a373b184452599516aa
Shubham-Kumar-cmd <kumar53shubham@gmail.com>
2024-03-05 16:54:03 +0530

    sending message from one user to other user and saving the data in Fire database

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { User } from '../../models/user';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+} from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss'
+  styleUrl: './user-list.component.scss',
 })
 export class UserListComponent {
-  userList:User[]=[];
-  constructor(private fireDb:AngularFireDatabase){
-    const list:AngularFireList<User>=this.fireDb.list('users');
-    list.valueChanges().subscribe(listOfUser=>{
-      this.userList=listOfUser
-    })
+  userList: User[] = [];
+  @Output() startChatEmitter: EventEmitter<string> = new EventEmitter();
+  constructor(private fireDb: AngularFireDatabase) {
+    const list: AngularFireList<User> = this.fireDb.list('users');
+    list.valueChanges().subscribe((listOfUser) => {
+      this.userList = listOfUser;
+    });
   }
-  
+
+  startChatChild(uid: string) {
+    this.startChatEmitter.next(uid);
+  }
 }
--- src/app/pages/chat-dashboard/chat-dashboard.component.ts
+++ src/app/pages/chat-dashboard/chat-dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import {
+  AngularFireDatabase,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { User } from '../../models/user';
+import { Message } from '../../models/message';
 
 @Component({
   selector: 'app-chat-dashboard',
@@ -13,6 +17,10 @@ import { User } from '../../models/user';
 })
 export class ChatDashboardComponent {
   currentUser: User | null = new User();
+  toUser!: User | null;
+  message: string = '';
+  chatRefNode: string = '';
+  oppChatRefNode: string = '';
   constructor(
     private fireAuth: AngularFireAuth,
     private fireDb: AngularFireDatabase,
@@ -32,5 +40,50 @@ export class ChatDashboardComponent {
   logoutFromChatDashboard() {
     this.authService.signoutFromFirebase();
     this.router.navigate(['/login']);
+  }
+
+  startChatParent(uid: string) {
+    console.log('parent ' + uid);
+
+    this.chatRefNode = `chats/${this.currentUser?.uid}****${uid}`; //chat from - to
+    this.oppChatRefNode = `chats/${uid}****${this.currentUser?.uid}`; //chat to - from
+
+    this.authService.getUserByUserId(uid).subscribe({
+      next: (user) => {
+        this.toUser = user;
+        console.log(this.toUser);
+      },
+      error: (error) => {
+        console.log(error);
+        this.toastr.error('Eroor in starting chat!!');
+      },
+    });
+  }
+
+  sendMessage(event: SubmitEvent) {
+    event.preventDefault();
+    if (this.message.trim() === '') {
+      return;
+    }
+    // send the message
+    console.log(this.message);
+    const msg = new Message();
+    msg.message = this.message;
+    msg.from = this.currentUser!.uid;
+    msg.to = this.toUser!.uid;
+
+    const chatRef: AngularFireObject<Message> = this.fireDb.object(
+      `${this.chatRefNode}/${new Date()}`
+    );
+    chatRef
+      .set(msg)
+      .then((data) => {
+        this.toastr.success('message send success!!');
+        this.message = '';
+      })
+      .catch((error) => {
+        this.toastr.error('error in sending message!!');
+        console.log(error);
+      });
   }
 }
